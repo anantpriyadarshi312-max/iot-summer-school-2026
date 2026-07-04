fc114a07881449f21bee672943f35d61fdff96ba
anantpriyadarshi312-max <anantpriyadarshi312@gmail.com>
2026-07-04 20:25:49 +0530

    Update digital_piano.ino

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,26 +1,21 @@
-const int buzzer = 8;
-const int buttons[4] = {2, 3, 4, 5};
-int notes[4] = {262, 294, 330, 349};
-
-void setup() {
-  for (int i = 0; i < 4; i++) {
-    pinMode(buttons[i], INPUT_PULLUP);
-  }
-  pinMode(buzzer, OUTPUT);
-}
-
 void loop() {
-  bool played = false;
+  int count = 0;
+  int lastPressed = -1;
 
   for (int i = 0; i < 4; i++) {
     if (digitalRead(buttons[i]) == LOW) {
-      tone(buzzer, notes[i]);
-      played = true;
-      break;
+      count++;
+      lastPressed = i;
     }
   }
 
-  if (!played) {
+  if (count == 0) {
     noTone(buzzer);
+  }
+  else if (count >= 2) {
+    tone(buzzer, 392); // Sol
+  }
+  else {
+    tone(buzzer, notes[lastPressed]);
   }
 }

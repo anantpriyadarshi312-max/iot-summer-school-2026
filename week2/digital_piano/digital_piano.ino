cc23870d44fcf3836c59c53d27bc354336e3f19b
anantpriyadarshi312-max <anantpriyadarshi312@gmail.com>
2026-07-04 20:23:59 +0530

    Added 4-key note playback

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,13 +1,26 @@
 const int buzzer = 8;
 const int buttons[4] = {2, 3, 4, 5};
+int notes[4] = {262, 294, 330, 349};
 
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(buttons[i], INPUT_PULLUP);
   }
-
   pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
+  bool played = false;
+
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(buttons[i]) == LOW) {
+      tone(buzzer, notes[i]);
+      played = true;
+      break;
+    }
+  }
+
+  if (!played) {
+    noTone(buzzer);
+  }
 }
